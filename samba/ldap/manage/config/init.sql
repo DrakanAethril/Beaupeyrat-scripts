e9e7f319461f3cf8a4ebc185241df760dd8de40c
Aethril <sebthar@gmail.com>
2026-07-03 18:52:50 +0200

    add init.sql with table definitions for ldap_manage_user and ldap_manage_group

---
+++ samba/ldap/manage/config/init.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS `ldap_manage_user` (
+    `id`          INT UNSIGNED     NOT NULL AUTO_INCREMENT,
+    `firstname`   VARCHAR(255)     NOT NULL,
+    `lastname`    VARCHAR(255)     NOT NULL,
+    `user_type`   VARCHAR(255)     NOT NULL,
+    `user_groups` VARCHAR(255)     NOT NULL DEFAULT '',
+    `action_type` VARCHAR(255)     NOT NULL,
+    `added_at`    DATETIME         NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `added_by`    VARCHAR(255)     NOT NULL DEFAULT 'direct',
+    `login`       VARCHAR(255)     DEFAULT NULL,
+    `password`    VARBINARY(255)   DEFAULT NULL,
+    `state`       TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `pid`         INT UNSIGNED     DEFAULT NULL,
+    `started_at`  DATETIME         DEFAULT NULL,
+    `ended_at`    DATETIME         DEFAULT NULL,
+    `log`         TEXT             DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11000 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `ldap_manage_group` (
+    `id`         INT UNSIGNED     NOT NULL AUTO_INCREMENT,
+    `name`       VARCHAR(255)     NOT NULL,
+    `added_at`   DATETIME         NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `added_by`   VARCHAR(255)     NOT NULL DEFAULT 'direct',
+    `state`      TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `pid`        INT UNSIGNED     DEFAULT NULL,
+    `started_at` DATETIME         DEFAULT NULL,
+    `ended_at`   DATETIME         DEFAULT NULL,
+    `log`        TEXT             DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=99100 DEFAULT CHARSET=utf8mb4;
